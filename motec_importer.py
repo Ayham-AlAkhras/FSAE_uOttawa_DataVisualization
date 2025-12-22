dfff739b25d66897f59f4e31965e2ec8bd66b70a
Samuel Drake <samueljhdrake@gmail.com>
2025-12-22 00:32:19 -0500

    motec_importer now stores metadata, doesnt use it for anything yet though

--- motec_importer.py
+++ motec_importer.py
@@ -14,44 +14,62 @@ class MoTeCImporter:
             errors="ignore")
         self.df = None              # DataFrame to hold the loaded data
         self.metadata = {}          # Metadata that is located at the top of the csv files
-        self.units = {}             # Units for each channel
+        self.channels = {}          # Channel items with their units
         self.header_index = None    # Index of the header row
     
     def _find_header_index(self):
+        """Finds the index of the header row starting with 'Time'."""
         
         target = '"Time"'
         encountered = False
         for i, line in enumerate(self.raw):
             if line.strip().startswith(target) and encountered is True:
+                self.header_index = i
                 return i
             elif line.strip().startswith(target):
                 encountered = True
         raise ValueError('Could not find header row starting with "Time". Is this a RaceStudio CSV export?')
 
-    def fetch_metadata(self, df):
-        
-        pass
-    
-    #Loads the file, checks for errors, ignores certain info (such as comments in MoTeC), saves the file
-    def load(self):
 
-        header_index = self._find_header_index()
+    def _fetch_metadata(self):
+        """Extracts metadata from the top of the CSV file."""
+        
+        metadata = {}
+        self.raw.seek(0)  # Reset file pointer to the beginning
+        for i, line in enumerate(self.raw):
+            if line.strip() == "":
+                continue
+            if i >= self.header_index - 1:
+                break
+            key, value = line.split(',', 1)
+            metadata[key] = value
+        self.metadata = metadata
+    
+    def load(self):
+        """
+        Loads the file, checks for errors, ignores certain 
+        info (such as comments in MoTeC), saves the file
+        """
+
         df = pd.read_csv(
             self.path, 
-            skiprows=header_index
+            skiprows=self._find_header_index(),
             )
-
         if df.empty:
             raise ValueError("CSV loaded, no data found")
         
         # First row after header is the units row
         units_row = df.iloc[0]
-        self.units = units_row.to_dict()
+        self.channels = units_row.to_dict()
 
         # Drop units row from data and convert to numeric where possible
         df = df.iloc[1:].reset_index(drop=True)
-        df = df.apply(pd.to_numeric, errors= "ignore")
-
+        try:
+            df = df.apply(pd.to_numeric)
+        except Exception as e:
+            raise ValueError(f"Error converting data to numeric: {e}")
+        
+        self._fetch_metadata()
         self.df = df
 
     #Validates to make sure it isn't all zero
